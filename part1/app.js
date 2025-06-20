7b67078a66adc957725ec7b7d44451287b50f516
Sean <a1826266@adelaide.edu.au>
2025-06-20 09:41:25 +0000

    [automatic commit] 2025-06-20 09:41:24 UTC

--- part1/app.js
+++ part1/app.js
@@ -45,6 +45,8 @@ async function main() {
         }
     });
     app.get('/api/walkrequests/open', async (req, res)=>{
-        
+        try{
+            const[rows] = await connection.query('')
+        }
     })
 }
