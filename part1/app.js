e928f6846a705fa0245c4593a1717b06683d6731
Sean <a1826266@adelaide.edu.au>
2025-06-20 09:29:10 +0000

    [automatic commit] 2025-06-20 09:29:10 UTC

--- part1/app.js
+++ part1/app.js
@@ -10,11 +10,11 @@ const config = {
 async function datainsert(connection) {
 try{
     await connection.query(`INSERT INTO Users (username, email, password_hash, role) VALUES
-    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-    ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-    ('carol123', 'carol@example.com', 'hashed101', 'owner'),
-    ('dogwalk', 'dogwalk@example.com', 'hashed202', 'walker'),
-    ('adam1', 'adam@example.com', 'hashed111', 'walker');`)
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed101', 'owner'),
+        ('dogwalk', 'dogwalk@example.com', 'hashed202', 'walker'),
+        ('adam1', 'adam@example.com', 'hashed111', 'walker');`)
     await connection.query(`INSERT INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
         ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
@@ -23,8 +23,7 @@ try{
         ((SELECT user_id FROM Users WHERE username = 'adam1'), 'Lorry', 'large');`)
     await connection.query(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
-        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
-        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands' 'open'),`)
