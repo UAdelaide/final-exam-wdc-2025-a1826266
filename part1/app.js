297f29951584590344253c9b0212da94f1617040
Sean <a1826266@adelaide.edu.au>
2025-06-20 09:22:11 +0000

    [automatic commit] 2025-06-20 09:22:11 UTC

--- part1/app.js
+++ part1/app.js
@@ -9,6 +9,7 @@ const config = {
 };
 async function datainsert(connection) {
 try{
-    await connection.query('INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed101', 'owner'), ('dogwalk', 'dogwalk@example.com', 'hashed202', 'walker'), ('adam1', 'adam@example.com', 'hashed111', 'walker');')
+    await connection.query('')
+    INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed101', 'owner'), ('dogwalk', 'dogwalk@example.com', 'hashed202', 'walker'), ('adam1', 'adam@example.com', 'hashed111', 'walker');
 }
 }
