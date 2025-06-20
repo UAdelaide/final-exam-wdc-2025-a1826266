c81d92ca4e4bec0610c4effc7abbae4e143b3bdd
Sean <a1826266@adelaide.edu.au>
2025-06-20 10:03:55 +0000

    [automatic commit] 2025-06-20 10:03:55 UTC

--- part1/app.js
+++ part1/app.js
@@ -9,19 +9,19 @@ const config = {
 };
 async function datainsert(connection) {
 try{
-    await connection.query(`INSERT INTO Users (username, email, password_hash, role) VALUES
+    await connection.query(`INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed101', 'owner'),
         ('dogwalk', 'dogwalk@example.com', 'hashed202', 'walker'),
         ('adam1', 'adam@example.com', 'hashed111', 'walker');`)
-    await connection.query(`INSERT INTO Dogs (owner_id, name, size) VALUES
+    await connection.query(`INSERT IGNORE INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
         ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
         ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Ben', 'medium'),
         ((SELECT user_id FROM Users WHERE username = 'dogwalk'), 'Chris', 'small'),
         ((SELECT user_id FROM Users WHERE username = 'adam1'), 'Lorry', 'large');`);
-    await connection.query(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+    await connection.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Ben'), '2025-07-10 10:00:00', 60, 'Wetlands', 'open'),
