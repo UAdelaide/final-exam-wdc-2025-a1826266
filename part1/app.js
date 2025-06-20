50b6cfaf4f4ec4604502fbbd3c1f82a420509630
Sean <a1826266@adelaide.edu.au>
2025-06-20 09:49:58 +0000

    [automatic commit] 2025-06-20 09:49:58 UTC

--- part1/app.js
+++ part1/app.js
@@ -56,7 +56,7 @@ async function main() {
     app.get('/api/walkers/summary', async (req, res)=>{
         try{
             const[rows] = await connection.query(`SELECT Users.user_id AS walker_id, Users.username, Users.email,
-                COUNT `)
+                COUNT(DISTINCT WalkApplications.request_id) AS `)
         }
     })
 }
