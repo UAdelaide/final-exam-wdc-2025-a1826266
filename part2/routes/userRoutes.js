5a1b66f1136180e9035dbfd4fc55f623b38c6dbe
Sean <a1826266@adelaide.edu.au>
2025-06-20 13:28:44 +0000

    [automatic commit] 2025-06-20 13:28:44 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -8,7 +8,7 @@ router.get('/:ownerId/dogs', async (req, res)=>{
     res.json(rows);
   }
   catch(err){
-    res.
+    res.status(500).json({error: 'failed'});
   }
 })
 // GET all users (for admin/testing)
