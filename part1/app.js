8d2a3e125343b4dba2dca34c199c0abfc08b1c46
Sean <a1826266@adelaide.edu.au>
2025-06-20 09:37:43 +0000

    [automatic commit] 2025-06-20 09:37:43 UTC

--- part1/app.js
+++ part1/app.js
@@ -35,5 +35,7 @@ try{
 async function main() {
     const connection = await mysql.createConnection(config);
     await datainsert(connection);
-    app.get('/api/dogs', async (req))
+    app.get('/api/dogs', async (req, res)=> {
+        try
+    })
 }
