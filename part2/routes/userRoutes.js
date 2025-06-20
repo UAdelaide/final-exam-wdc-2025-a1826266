5745937d110cf8fe987d0716157b039f57f62986
Sean <a1826266@adelaide.edu.au>
2025-06-20 11:58:46 +0000

    [automatic commit] 2025-06-20 11:58:46 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,6 +48,9 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
+    req.session.user = {
+      user_id: 
+    }
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
