235ce7cdac396d22172ac4bb963a0385096661aa
Sean <a1826266@adelaide.edu.au>
2025-06-20 14:40:42 +0000

    [automatic commit] 2025-06-20 14:40:42 UTC

--- part2/app.js
+++ part2/app.js
@@ -14,6 +14,6 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/api', dog)
 // Export the app instead of listening here
 module.exports = app;
