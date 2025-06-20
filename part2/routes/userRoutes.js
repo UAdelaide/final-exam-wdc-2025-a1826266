bcdaddbcbfc7635f40780571b7830108345843b1
Sean <a1826266@adelaide.edu.au>
2025-06-20 12:31:32 +0000

    [automatic commit] 2025-06-20 12:31:32 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,7 +38,7 @@ router.get('/me', (req, res) => {
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
+// Changed from input 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
